a73ad089f0ec990460f5afc69e080244704efa51
ParulAjudiya <parul.ajudiya@gmail.com>
2018-10-03 13:14:28 +0300

    Added Call function

--- topFXtestv1/src/Command/IntegrateStocksDataCommand.php
+++ topFXtestv1/src/Command/IntegrateStocksDataCommand.php
@@ -77,5 +77,41 @@ class IntegrateStocksDataCommand extends Command
 				//echo $lastid;
 			}
 			$io->out("Completed -- Save data of ".$stockCode." in SQLlite \n");
-	}	
+	}
+	
+	private function callAPI($method, $url, $data){
+		   
+		   $curl = curl_init();
+		   switch ($method){
+			  case "POST":
+				 curl_setopt($curl, CURLOPT_POST, 1);
+				 if ($data)
+					curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+				 break;
+			  case "PUT":
+				 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+				 if ($data)
+					curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+				 break;
+			  default:
+				 if ($data)
+					$url = sprintf("%s?%s", $url, http_build_query($data));
+		   }
+		   // OPTIONS:
+		   curl_setopt($curl, CURLOPT_URL, $url);
+		   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+			  'APIKEY: 111111111111111111111',
+			  'Content-Type: application/json',
+		   ));
+		   
+		   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+		   // EXECUTE:
+		   $result = curl_exec($curl);
+		   if(!$result){die("Connection Failure");}
+		   curl_close($curl);
+		   
+		   return $result;
+		}
 }
